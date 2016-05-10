0b34409b99c0152a2a0904cd05f71a023213f40a
Rammun <russslllan@mail.ru>
2016-05-10 06:35:37 +0300

    Шаг1. Создал проект, добавил интерфейс.

---
+++ Snake.Logic/Interfaces/IGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Logic.Interfaces
+{
+    public interface IGame
+    {
+        public void Initialization();
+        public void Start();
+        public void Step();
+        public void Stop();
+    }
+}
---
+++ Snake.Logic/SnakeGame.cs
@@ -0,0 +1,32 @@
+﻿using Snake.Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Logic
+{
+    public class SnakeGame : IGame
+    {
+        public void Initialization()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Step()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
